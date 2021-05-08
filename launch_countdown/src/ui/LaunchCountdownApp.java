e8d1ee5fe7491595bb90a78c2af3869530cf0b5c
Thomas Dahlinghaus <twdahlinghaus@gmail.com>
2021-05-07 23:42:20 -0400

    quick practice to see if this setup works

---
+++ launch_countdown/src/ui/LaunchCountdownApp.java
@@ -0,0 +1,21 @@
+package ui;
+
+public class LaunchCountdownApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Launch Countdown App!!");
+		System.out.println();
+		System.out.println("We are ready for launch in T minus ");
+		
+		for (int count = 10; count >= 0; count--)
+			System.out.println(count);
+		
+		System.out.println("All engines go!");
+		System.out.println("We have lift off!!");
+			
+		
+		System.out.println("Up, up and awaaaaaaay!!");
+
+	}
+
+}
